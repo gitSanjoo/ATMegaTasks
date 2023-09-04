7d3ef71a120365f265ebdd0065b233e872748abc
devender.kumar6 <devender.kumar6@olaelectric>
2023-09-05 00:20:37 +0530

    successfully created a toggle button to change language on a text view

--- app/src/main/java/com/sanjoo/atmegatasks/DarkLightModeSwitching.kt
+++ app/src/main/java/com/sanjoo/atmegatasks/DarkLightModeSwitching.kt
@@ -1,11 +1,16 @@
 package com.sanjoo.atmegatasks
 
+import android.content.SharedPreferences
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import android.widget.ToggleButton
-import com.sanjoo.atmegatasks.databinding.ActivityMainBinding
+import java.util.Locale
 
 class DarkLightModeSwitching : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,16 +18,42 @@ class DarkLightModeSwitching : AppCompatActivity() {
 //        val binding:ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
 //        val view: View =binding.root
 //        setContentView(view)
-//
+
+        loadLocale()
 
         setContentView(R.layout.activity_dark_light_mode_switching)
-        var tb:ToggleButton = findViewById(R.id.toggle_btn_change_language)!!
+        var et:TextView=findViewById((R.id.et_word))
+        var tb: ToggleButton = findViewById(R.id.toggle_btn_change_language)!!
+
+
         tb.setOnClickListener(View.OnClickListener {
-            changeLanguage()
+
+            if (tb.isChecked) {
+                setLocate("hi")
+                recreate()
+            }else{
+                setLocate("en")
+                recreate()
+            }
         })
     }
+    fun setLocate(lan: String){
+        var locale=Locale(lan)
+        var config=Configuration()
+        config.locale=locale
 
-    private fun changeLanguage() {
-        TODO("Not yet implemented")
+        baseContext.resources.updateConfiguration(config,baseContext.resources.displayMetrics)
+
+        val editor:SharedPreferences.Editor=getSharedPreferences("setting", MODE_PRIVATE).edit()
+        editor.putString("app_lan",lan)
+        editor.apply()
+
+    }
+    fun loadLocale(){
+        val sharerdPrefrence:SharedPreferences=getSharedPreferences("setting", MODE_PRIVATE)
+        val lang: String? =sharerdPrefrence.getString("app_lan","")
+        if (lang != null) {
+            setLocate(lang)
+        }
     }
 }
