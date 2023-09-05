146c2da132f8b882bfad337937d26af6279da5df
Sanju <swamisanjoo@gmail.com>
2023-09-05 22:35:44 +0530

    created activity CurrentLocationActivity.kt

---
+++ app/src/main/java/com/sanjoo/atmegatasks/CurrentLocationActivity.kt
@@ -0,0 +1,11 @@
+package com.sanjoo.atmegatasks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CurrentLocationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_current_location)
+    }
+}
