d7f6aa6d5c85f86d747b1155d6c937b0251074b1
Sanju <swamisanjoo@gmail.com>
2023-09-05 21:12:41 +0530

    mode swithing achieved

--- app/src/main/java/com/sanjoo/atmegatasks/DarkLightModeSwitching.kt
+++ app/src/main/java/com/sanjoo/atmegatasks/DarkLightModeSwitching.kt
@@ -1,5 +1,7 @@
 package com.sanjoo.atmegatasks
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.SharedPreferences
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
@@ -10,15 +12,46 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import android.widget.ToggleButton
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import java.util.Locale
 
 class DarkLightModeSwitching : AppCompatActivity() {
+
+    private lateinit var modeSwitch:SwitchCompat
+    private var nightMode:Boolean=false
+    private var modeEditor:SharedPreferences.Editor?=null
+    private var sharedPreferencesMode:SharedPreferences?=null
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         loadLocale()
 
         setContentView(R.layout.activity_dark_light_mode_switching)
+ //code for mode change
+        modeSwitch=findViewById(R.id.mode_switch)!!
+        sharedPreferencesMode=getSharedPreferences("MODE",Context.MODE_PRIVATE)
+        nightMode=sharedPreferencesMode?.getBoolean("night",false)!!
+        if (nightMode){
+            modeSwitch.isChecked=true
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+        modeSwitch.setOnCheckedChangeListener{compoundButton,state->
+            if (nightMode){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                modeEditor=sharedPreferencesMode?.edit()
+                modeEditor?.putBoolean("night",false)
+            }
+            else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                modeEditor=sharedPreferencesMode?.edit()
+                modeEditor?.putBoolean("night",true)
+            }
+            modeEditor?.apply()
+        }
+
+//code for language translate
+
         var et:TextView=findViewById((R.id.et_word))
         var tb: ToggleButton = findViewById(R.id.toggle_btn_change_language)!!
 
