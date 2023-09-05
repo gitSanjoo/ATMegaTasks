55d0dbd50fb049c37a049811f97fc450b85e395a
Sanju <swamisanjoo@gmail.com>
2023-09-05 10:32:16 +0530

    trying to create toggle btn to change language no changes

--- app/src/main/java/com/sanjoo/atmegatasks/DarkLightModeSwitching.kt
+++ app/src/main/java/com/sanjoo/atmegatasks/DarkLightModeSwitching.kt
@@ -1,16 +1,11 @@
 package com.sanjoo.atmegatasks
 
-import android.content.SharedPreferences
-import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
 import android.widget.ToggleButton
-import java.util.Locale
+import com.sanjoo.atmegatasks.databinding.ActivityMainBinding
 
 class DarkLightModeSwitching : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,42 +13,16 @@ class DarkLightModeSwitching : AppCompatActivity() {
 //        val binding:ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
 //        val view: View =binding.root
 //        setContentView(view)
-
-        loadLocale()
+//
 
         setContentView(R.layout.activity_dark_light_mode_switching)
-        var et:TextView=findViewById((R.id.et_word))
-        var tb: ToggleButton = findViewById(R.id.toggle_btn_change_language)!!
-
-
+        var tb:ToggleButton = findViewById(R.id.toggle_btn_change_language)!!
         tb.setOnClickListener(View.OnClickListener {
-
-            if (tb.isChecked) {
-                setLocate("hi")
-                recreate()
-            }else{
-                setLocate("en")
-                recreate()
-            }
+changeLanguage()
         })
     }
-    fun setLocate(lan: String){
-        var locale=Locale(lan)
-        var config=Configuration()
-        config.locale=locale
 
-        baseContext.resources.updateConfiguration(config,baseContext.resources.displayMetrics)
-
-        val editor:SharedPreferences.Editor=getSharedPreferences("setting", MODE_PRIVATE).edit()
-        editor.putString("app_lan",lan)
-        editor.apply()
-
-    }
-    fun loadLocale(){
-        val sharerdPrefrence:SharedPreferences=getSharedPreferences("setting", MODE_PRIVATE)
-        val lang: String? =sharerdPrefrence.getString("app_lan","")
-        if (lang != null) {
-            setLocate(lang)
-        }
+    private fun changeLanguage() {
+        TODO("Not yet implemented")
     }
 }
