1515423e33a73277ee3d3cf908d3d034d99e1bc5
devender.kumar6 <devender.kumar6@olaelectric>
2023-09-06 22:56:23 +0530

    current location can be seen by a notification(bcz foreground service is used)

--- app/src/main/java/com/sanjoo/atmegatasks/CurrentLocationActivity.kt
+++ app/src/main/java/com/sanjoo/atmegatasks/CurrentLocationActivity.kt
@@ -1,11 +1,37 @@
 package com.sanjoo.atmegatasks
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.core.app.ActivityCompat
 
 class CurrentLocationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+            ),0
+            )
         setContentView(R.layout.activity_current_location)
+        val btnStart:Button=findViewById(R.id.start)
+        val btnStop:Button=findViewById(R.id.stop)
+
+        btnStart.setOnClickListener(View.OnClickListener {
+            Intent(applicationContext,LocationService::class.java).apply {
+                action=LocationService.ACTION_START
+                startService(this)
+            }
+        })
+        btnStop.setOnClickListener(View.OnClickListener {
+            Intent(applicationContext,LocationService::class.java).apply {
+                action=LocationService.ACTION_STOP
+                startService(this )
+            }
+        })
     }
 }
