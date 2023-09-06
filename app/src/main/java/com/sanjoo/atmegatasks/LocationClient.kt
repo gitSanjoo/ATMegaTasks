e4506a8e17aa41ca3c85b4974831cec51f2bcd75
Sanju <swamisanjoo@gmail.com>
2023-09-06 20:23:25 +0530

    create interface ,file and a kotlin class

---
+++ app/src/main/java/com/sanjoo/atmegatasks/LocationClient.kt
@@ -0,0 +1,10 @@
+package com.sanjoo.atmegatasks
+
+import android.location.Location
+import kotlinx.coroutines.flow.Flow
+
+interface LocationClient {
+    fun getLocationUpdate(interval:Long): Flow<Location>
+
+    class LocationException(message:String):Exception()
+}
